3979dec7038253548bd8fb8d37e7421760b26d2d
Brian Dennis <briandennis321@gmail.com>
2016-01-14 18:29:06 -0600

    changed url for api

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -19419,7 +19419,7 @@ var PrimaryEntry = React.createClass({
     var getMovie = function (name) {
 
       //format URL
-      var movieUrl = 'http://whattosee.briandennis.io/api?movie=' + name;
+      var movieUrl = 'http://movies.briandennis.io/api?movie=' + name;
 
       //get the movie via http request
       var request = new XMLHttpRequest();
@@ -19748,7 +19748,7 @@ var MainContent = React.createClass({
       var getBoxOffice = function (name) {
 
         //format URL
-        var movieUrl = 'http://whattosee.briandennis.io/api/out';
+        var movieUrl = 'http://movies.briandennis.io/api/out';
 
         //get the movie via http request
         var request = new XMLHttpRequest();
--- public/js/main.js
+++ public/js/main.js
@@ -116,7 +116,7 @@ var PrimaryEntry = React.createClass({
     var getMovie = function(name){
 
       //format URL
-      var movieUrl = 'http://whattosee.briandennis.io/api?movie=' + name ;
+      var movieUrl = 'http://movies.briandennis.io/api?movie=' + name ;
 
       //get the movie via http request
       var request = new XMLHttpRequest();
@@ -348,7 +348,7 @@ var MainContent = React.createClass({
       var getBoxOffice = function(name){
 
         //format URL
-        var movieUrl = 'http://whattosee.briandennis.io/api/out';
+        var movieUrl = 'http://movies.briandennis.io/api/out';
 
         //get the movie via http request
         var request = new XMLHttpRequest();
